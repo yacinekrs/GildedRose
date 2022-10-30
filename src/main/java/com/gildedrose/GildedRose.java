3124d2443db3a244dd1fdf8f37a27c85addaec6b
KHORSI Yacine <yacinekhorsi1@gmail.com>
2022-10-30 12:45:06 +0100

    conjured

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,42 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item: items ) {
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")){
-                return;
-            }
-            switch (item.name) {
-                case"Aged Brie":
-                    item.quality ++;
-                    if (item.sellIn < 0) {
-                        item.quality ++;
-                    }   
-                    break;
-                case"Backstage passes to a TAFKAL80ETC concert":
-                    item.quality ++ ;
-                    if (item.sellIn < 10) {
-                        item.quality ++ ;
-                    }
-                    if (item.sellIn < 5) {
-                        item.quality ++ ;
-                    }
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }   
-                    break;
-            
-                default:
-                    item.quality--;
-                    if (item.sellIn < 0) {
-                        item.quality--;
-                    }
-                    break;
-            }
-            if (item.quality > 50){
-                item.quality = 50;
-            }
-            if (item.quality < 0){
-                item.quality = 0;
-            }
+           item.updateQuality();
+        }
     }
-}
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,55 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+    
+    public void updateQuality(){
+        if (this.name.equals("Sulfuras, Hand of Ragnaros")){
+            return;
+        }
+
+        this.sellIn --;
+        switch (this.name) {
+            case "Aged Brie":
+                this.quality ++;
+                if (this.sellIn < 0) {
+                    this.quality ++;
+                }   
+                break;
+
+            case "Backstage passes to a TAFKAL80ETC concert":
+                this.quality ++ ;
+                if (this.sellIn < 10) {
+                    this.quality ++ ;
+                }
+                if (this.sellIn < 5) {
+                    this.quality ++ ;
+                }
+                if (this.sellIn < 0) {
+                    this.quality = 0;
+                }   
+                break;
+
+            case "Conjured":
+                if(this.sellIn >= 0){
+                    this.quality-= 2;
+                }
+                if(this.sellIn < 0){
+                    this.quality-= 4;
+                }
+                break;
+        
+            default:
+                this.quality--;
+                if (this.sellIn < 0) {
+                    this.quality--;
+                }
+                break;
+        }
+        if (this.quality > 50){
+            this.quality = 50;
+        }
+        if (this.quality < 0){
+            this.quality = 0;
+        }
+    }
 }
